f0e1792aa6eeb0f5a29e8798fda8f703c768c995
menorziin <williamsilvapppl@gmail.com>
2022-12-31 11:16:11 +0000

    liber: Drop servicetracker HAL
    Change-Id: I18e34800f58d36467173eed2f7539cfb5efb212e

--- device.mk
+++ device.mk
@@ -422,10 +422,6 @@ PRODUCT_PACKAGES += \
     android.hardware.sensors@2.0-service.multihal \
     libsensorndkbridge
 
-# Servicetracker
-PRODUCT_PACKAGES += \
-    vendor.qti.hardware.servicetracker@1.2.vendor
-
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
