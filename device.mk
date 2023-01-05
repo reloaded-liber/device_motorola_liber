3062d6d2ee8bbe95bca5515e77fbd9e726419c48
menorziin <williamsilvapppl@gmail.com>
2023-01-05 08:26:15 -0300

    liber: Downrev audio HAL to 6.0
    Change-Id: Id1a720ce852116ca450a150819a7a656c2a5bc32

--- device.mk
+++ device.mk
@@ -110,8 +110,8 @@ PRODUCT_COPY_FILES += \
 
 # Audio
 PRODUCT_PACKAGES += \
-    android.hardware.audio@7.1-impl \
-    android.hardware.audio.effect@7.0-impl \
+    android.hardware.audio@6.0-impl \
+    android.hardware.audio.effect@6.0-impl \
     android.hardware.audio.service \
     android.hardware.soundtrigger@2.2-impl \
     audio.primary.default \
