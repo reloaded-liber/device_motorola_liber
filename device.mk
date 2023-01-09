42475d19693a6a20a95212a86bc800114ebbab87
menorziin <williamsilvapppl@gmail.com>
2023-01-09 04:11:31 -0300

    liber: Import GPS configs from hanoip
    Change-Id: I1f5313b0608f77e9b3bb011efe92588c7510e324

--- device.mk
+++ device.mk
@@ -236,6 +236,8 @@ PRODUCT_PACKAGES += \
 
 # GPS
 LOC_HIDL_VERSION := 4.0
+PRODUCT_COPY_FILES += \
+    $(call find-copy-subdir-files,*,$(LOCAL_PATH)/configs/gps/,$(TARGET_COPY_OUT_VENDOR)/etc)
 
 # Health
 PRODUCT_PACKAGES += \
